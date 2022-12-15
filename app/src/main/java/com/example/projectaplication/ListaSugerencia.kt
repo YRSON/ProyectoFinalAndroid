5c9c3bb7ee723737a4bf863c9fdd34250b125768
LBRAVO7 <leo712bc@gmail.com>
2022-12-14 20:42:27 -0500

    cambiosleonardo1

--- app/src/main/java/com/example/projectaplication/ListaSugerencia.kt
+++ app/src/main/java/com/example/projectaplication/ListaSugerencia.kt
@@ -1,11 +1,33 @@
 package com.example.projectaplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
-class ListaSugerencia : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_sugerencia)
+class ListaSugerencia : Fragment() {
+
+    override  fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val view = inflater.inflate(R.layout.fragment_lista_sugerencia, container, false)
+        val prevBtn: Button =view.findViewById(R.id.btnregresarsugerencias)
+        prevBtn.setOnClickListener {
+            val fragment = Sugerencias()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.frame_layout, fragment)?.commit()
+        }
+
+        return view
+
     }
 }
